2a5328b178ea0f911f550a6b3cfc35f2af246b28
olesh <olegkirashchuk@gmail.com>
2021-08-29 22:57:04 +0300

    add credits screen

---
+++ views/Credits.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default () => (
+  <View style={{ paddingVertical: 10, paddingHorizontal: 20 }}>
+    <Text style={{ color: "#FFF" }}>kek lol</Text>
+  </View>
+)
